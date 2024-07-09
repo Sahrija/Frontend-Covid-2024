fc94b3b5bce462c11cc92eaff9df2a4387d8c201
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-07-10 01:29:50 +0700

    feat: new pages

---
+++ stay-safe/src/pages/Indonesia.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Hero from '../components/Hero/Hero'
+
+const Indonesia = () => {
+    const [indonesia, setIndonesia] = useState({})
+    
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default Indonesia

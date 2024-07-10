d64a350b4298f49798f0876acad29654c1f0e3ff
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-07-11 01:16:16 +0700

    fix: restyle datacard

--- stay-safe/src/components/DataCard/DataCard.jsx
+++ stay-safe/src/components/DataCard/DataCard.jsx
@@ -5,11 +5,11 @@ function DataCard({ record, index }) {
     const totalLabelColor = ['text-green-500', 'text-sky-500', 'text-red-500']
     const total = formatThousand(record.total);
 
-    return <div className='flex flex-col sm:flex-1 bg-white shadow-md p-10 rounded text-center'>
-        <span className='text-3xl'>
+    return <div className='flex flex-col sm:flex-1 bg-white shadow-md px-4 sm:px-8 py-4 sm:py-10 rounded text-center'>
+        <span className='text-xl md:text-3xl capitalize'>
             {record.status}
         </span>
-        <span className={`break-all text-5xl font-bold ${totalLabelColor[index]}`}>
+        <span className={`break-all text-3xl md:text-4xl font-bold ${totalLabelColor[index]} break-keep`}>
             {total}
         </span>
     </div>;

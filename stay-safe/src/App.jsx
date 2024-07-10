da6fd5cbebedb63d08e1ef431f087a55f08ff35f
Morning Coffee <114372392+Sahrija@users.noreply.github.com>
2024-07-11 00:01:54 +0700

    feat: routes

--- stay-safe/src/App.jsx
+++ stay-safe/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './layouts/Layout';
+import Indonesia from './pages/Indonesia';
+import Province from './pages/Province';
+import About from './pages/About';
 
 function App() {
 
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />}></Route>
+          <Route path='indonesia' element={<Indonesia />}></Route>
+          <Route path='province' element={<Province />}></Route>
+          <Route path='about' element={<About />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
